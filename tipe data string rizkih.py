dfa87460485b787ab3f517b9bd03dbc30dd3bc8f
Rizkihdytllah <rizkihidayatullah1906@gmail.com>
2024-09-27 11:01:48 +0700

    Add files via upload

---
+++ tipe data string rizkih.py
@@ -0,0 +1,13 @@
+nama_depan = "Rizki"
+nama_belakang = 'Hidayatullah'
+nama_lengkap = nama_depan + '' + nama_belakang
+usia = 19
+alamat = 'AIR GEGAS'
+kata_mutiara = "Jangan lupa untuk tidur"
+
+print(nama_lengkap, '(' + usia + ')', ', ', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print(',Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
